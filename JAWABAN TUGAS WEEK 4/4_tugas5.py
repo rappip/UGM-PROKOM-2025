95618e35a362b687ccc34bdfe68eb265ee562a49
rappip <rafifnandapramuditya.r@mail.ugm.ac.id>
2025-09-12 08:12:18 +0700

    Add files via upload

---
+++ JAWABAN TUGAS WEEK 4/4_tugas5.py
@@ -0,0 +1,32 @@
+nama = str(input('masukkan nama : '))
+nilai = int(input('masukkan nilai : '))
+if nilai >= 70 :
+    print('lulus')
+else :
+    print('gagal')
+nama = str(input('masukkan nama : '))
+nilai = int(input('masukkan nilai : '))
+if nilai >= 70 :
+    print('lulus')
+else :
+    print('gagal')
+nama = str(input('masukkan nama : '))
+nilai = int(input('masukkan nilai : '))
+if nilai >= 70 :
+    print('lulus')
+else :
+    print('gagal')
+nama = str(input('masukkan nama : '))
+nilai = int(input('masukkan nilai : '))
+if nilai >= 70 :
+    print('lulus')
+else :
+    print('gagal')
+nama = str(input('masukkan nama : '))
+nilai = int(input('masukkan nilai : '))
+if nilai >= 70 :
+    print('lulus')
+else :
+    print('gagal')
+
+    
